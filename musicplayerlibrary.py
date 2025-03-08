685fe0609888da69201789b42dc216d7280d555d
Shubham-Kumar-1 <bt23cse162@shivalikcollege.edu.in>
2025-03-08 19:53:51 +0530

    A library of musics

---
+++ musicplayerlibrary.py
@@ -0,0 +1,16 @@
+music ={
+     "dil":"https://www.youtube.com/watch?v=p1qTMyhLsAE",
+     "fallen":"https://www.youtube.com/watch?v=v2Qr_UFuX5o",
+     "ishaq":"https://www.youtube.com/watch?v=3KXZduvOfDo",
+     "jaaniya":"https://www.youtube.com/watch?v=FyBjKLBqZhM&list=PLsygL79XhOK4GgcUKhZx_jdh4iVYVZy0k&index=6",
+     "kinna Sonna":"https://www.youtube.com/watch?v=Y3Ap5X9jW0s&list=PLsygL79XhOK4GgcUKhZx_jdh4iVYVZy0k&index=9",
+     "mere ":"https://www.youtube.com/watch?v=rhP7QSWYY8c&list=PLsygL79XhOK4GgcUKhZx_jdh4iVYVZy0k&index=4",
+     "naina":"https://www.youtube.com/watch?v=e1zn5T0K45o",
+     "pyaar":"https://www.youtube.com/watch?v=VbLGUEZsmBo",
+     "raanjhana":"https://www.youtube.com/watch?v=D5dZAjn_8tw&list=PLsygL79XhOK4GgcUKhZx_jdh4iVYVZy0k&index=11",
+     "rangreja":"https://www.youtube.com/watch?v=qqA40OTvxbM",
+     "sharara":"https://www.youtube.com/watch?v=4cw3x0tU-pk",
+     "sunrise":"https://www.youtube.com/watch?v=f9acDGddKr0&list=PL9bw4S5ePsEENcvVDyVQ-fWzC-kNbUkQy",
+     "wada":"https://www.youtube.com/watch?v=VbLGUEZsmBo"
+
+}
